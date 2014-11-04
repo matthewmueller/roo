cb48762d263e30a6e877a9ff500f48daf3c57f61
Matthew Mueller <mattmuelle@gmail.com>
2014-11-04 19:37:05 +1000

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,16 @@
+// /**
+//  * Module Dependencies
+//  */
+
+// var bundle = require('duo-bundle')(__dirname);
+// var serve = require('../');
+
+// serve(__dirname)
+//   .compress()
+//   .duo('test.{js,css}')
+//   .static('build')
+//   .get('/', 'test.jade')
+//   .directory()
+//   .listen(function() {
+//     console.log('listening on: %s', this.address().port);
+//   });
---
+++ test/test.js
@@ -0,0 +1,5 @@
+console.log('hI!');
+var jquery = require('jquery/jquery@2.1.1:dist/jquery.js');
+console.log(jquery);
+
+console.log('come on.zz');

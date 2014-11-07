593f201ce130edb97bb9887bb20a270d8377d46f
Matthew Mueller <mattmuelle@gmail.com>
2014-11-07 04:14:37 -0800

    replaced kroute with kr. fixed roo.directory()

--- test/index.js
+++ test/index.js
@@ -1,16 +1,21 @@
-// /**
-//  * Module Dependencies
-//  */
+/**
+ * Module Dependencies
+ */
 
-// var bundle = require('duo-bundle')(__dirname);
-// var serve = require('../');
+var bundle = require('duo-bundle')(__dirname);
+var roo = require('../');
 
-// serve(__dirname)
-//   .compress()
-//   .duo('test.{js,css}')
-//   .static('build')
-//   .get('/', 'test.jade')
-//   .directory()
-//   .listen(function() {
-//     console.log('listening on: %s', this.address().port);
-//   });
+function *auth(next) {
+  console.log('authing!');
+  yield next;
+}
+
+roo(__dirname)
+  .compress()
+  .duo('test.{js,css}')
+  .static('build')
+  .get('/', auth, 'test.jade')
+  .directory()
+  .listen(5000, function() {
+    console.log('listening on: %s', this.address().port);
+  });
